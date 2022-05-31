efa06224982369c1c4274956d3ff098f25f78a62
Pablo <pabloarancibia.dw@gmail.com>
2022-05-31 01:51:16 -0300

    wifi & mqtt connect ok, con limitaciones

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,24 @@ String UIDCaracteres;//UID de tarjeta rfid
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+void ledblink(int largo){
+  digitalWrite(led, HIGH);
+  delay(largo);
+  digitalWrite(led, LOW);
+
+}
+
+//Sonido del buzzer
+void bip(int largo, int toques){
+  for (int t=0;t<toques;t++){
+    digitalWrite(buzzer, HIGH);
+    delay(largo);
+    digitalWrite(buzzer, LOW);
+    delay(200);//delay entre toques
+  } 
+    
+}
+
 //RFID WIFI MQTT SETUP
 void setup() {
   pinMode(buzzer, OUTPUT);
@@ -27,23 +45,34 @@ void setup() {
   Serial.begin(115200); // Velocidad del terminal serial
 
   //wifi
-  const char* ssid = "ZulemayPablo";
-  const char* password = "P4bl0yZul3";
+  const char* ssid = "nodemcu";
+  //const char* password = "1ns3gur4";
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid);
+
+  
 
   while (WiFi.status() != WL_CONNECTED) {   
       Serial.println("Connecting to WiFi..");
       Serial.println(WiFi.status());
+      Serial.println(ssid);
+      Serial.println();
+      Serial.print("ESP Board MAC Address:  ");
+      Serial.println(WiFi.macAddress());
+      ledblink(300);
       delay(1000);
   }
  
   Serial.println("Connected to the WiFi network");
   Serial.println(WiFi.localIP());
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  ledblink(5000);
+  bip(3000,2);
 
   //mqtt
-  const char* mqttServer = "192.168.2.120";
+  const char* mqttServer = "192.168.2.106";
   const int mqttPort = 1883;
   const char* mqttUser = "admin";
   const char* mqttPassword = "4dm1n/t3st";
@@ -53,7 +82,8 @@ void setup() {
   while (!client.connected()) {
       Serial.println("Connecting to MQTT...");
   
-      if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+      // if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+      if (client.connect("esp32")) {
   
         Serial.println("connected");  
   
@@ -70,16 +100,8 @@ void setup() {
   client.publish("esp/test", "Hello from ESP32");
   }
 
-//Sonido del buzzer
-void bip(int largo, int toques){
-  for (int t=0;t<toques;t++){
-    digitalWrite(buzzer, HIGH);
-    delay(largo);
-    digitalWrite(buzzer, LOW);
-    delay(200);//delay entre toques
-  } 
-    
-}
+
+
 
 void loop() {
   if (rfid.PICC_IsNewCardPresent())  // Hay una nueva tarjeta presente
