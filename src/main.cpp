f21a0d64c92cce8ff6776e8c9dd4dc876e4f3b2f
Pablo <pabloarancibia.dw@gmail.com>
2022-08-29 23:08:58 -0300

    refactor - sub error nodo mostrador

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,25 @@ const int led = 2;//Led onboard del esp32
 
 MFRC522 rfid(pinSDA, pinRST);
 String UIDCaracteres;//UID de tarjeta rfid
+
+// NODO CONFIG
 String topic_str = NODO_TOPIC;//topic
+String topic_error = TOPIC_ERROR;//topic
+
+// MQTT CONF
+const char* mqttServer = MQTT_SERVER;
+const int mqttPort = MQTT_PORT;
+const char* mqttUser = MQTT_USER;
+const char* mqttPassword = MQTT_PASSWORD;
 
 
+// WIFI CONF
 WiFiClient espClient;
+
 PubSubClient client(espClient);
 
+
+// LED function
 void ledblink(int largo){
   digitalWrite(led, HIGH);
   delay(largo);
@@ -27,7 +40,7 @@ void ledblink(int largo){
 
 }
 
-//Sonido del buzzer
+// buzzer function
 void bip(int largo, int toques){
   for (int t=0;t<toques;t++){
     digitalWrite(buzzer, HIGH);
@@ -37,88 +50,13 @@ void bip(int largo, int toques){
   } 
     
 }
-void mqttConnect(){
-  //mqtt
-  const char* mqttServer = MQTT_SERVER;
-  const int mqttPort = MQTT_PORT;
-  const char* mqttUser = MQTT_USER;
-  const char* mqttPassword = MQTT_PASSWORD;
 
-  client.setServer(mqttServer, mqttPort);
-  
-  while (!client.connected()) {
-      Serial.println("Connecting to MQTT...");
-  
-      // if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
-      if (client.connect(NODO_NOMBRE)) {
-
-        Serial.print("conectado a mqtt en: ");
-        Serial.print(MQTT_SERVER);
-        Serial.print(":");
-        Serial.print(MQTT_PORT);
-        Serial.println();
-
-        
-      } else {
-  
-        Serial.print("failed with state ");
-        Serial.print(client.state());
-        delay(2000);
-  
-      }
-  }
-
-    
-  client.publish("esp/test", "Hello from ESP32+RFIDrc522");
-}
-
-//RFID WIFI MQTT SETUP
-void setup() {
-  pinMode(buzzer, OUTPUT);
-  pinMode(led, OUTPUT);
-  SPI.begin();
-  rfid.PCD_Init();//Inicilializar lector
-  Serial.begin(115200); // Velocidad del terminal serial
-
-  //wifi
-  const char* ssid = WIFI_LOCAL_SSID;
-  //const char* password = WIFI_LOCAL_PASS;
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid);
-
-  
-
-  while (WiFi.status() != WL_CONNECTED) {   
-      Serial.println("Connecting to WiFi..");
-      Serial.println(WiFi.status());
-      Serial.println(ssid);
-      Serial.println();
-      Serial.print("ESP Board MAC Address:  ");
-      Serial.println(WiFi.macAddress());
-      ledblink(300);
-      delay(1000);
-  }
- 
-  Serial.println("Connected to the WiFi network");
-  Serial.println(WiFi.localIP());
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-  ledblink(5000);
-  bip(200,2);//conexion wifi exitosa
-
-  //llamar a coneccion de mqtt
-  mqttConnect();
-  }
-
-
-
-
-void loop() {
-  while (!client.connected()) {
-    Serial.print(client.state());
-    mqttConnect();
-  }
+// publicación cuando detecta una tarjeta
+void PublishMqtt() {
+  // while (!client.connected()) {
+  //   Serial.print(client.state());
+  //   mqttConnect();
+  // }
   
   if (rfid.PICC_IsNewCardPresent())  // Hay una nueva tarjeta presente
     {
@@ -155,4 +93,172 @@ void loop() {
       } 
     }
   UIDCaracteres="";  
+}
+
+// realiza las suscripción a los topic
+void SuscribeMqtt()
+{
+    char topic_error_buff[100];
+    topic_error.toCharArray(topic_error_buff,topic_error.length()+1);
+    client.subscribe(topic_error_buff);
+    Serial.print("suscripto a topic: ");
+    Serial.print(topic_error_buff);
+    Serial.println();
+}
+
+// callback a ejecutar cuando se recibe un mensaje
+void OnMqttReceived(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Received on ");
+    Serial.print(topic);
+    Serial.print(": ");
+    String content = "";
+    for (size_t i = 0; i < length; i++)
+    {
+        content.concat((char)payload[i]);
+    }
+    //imprimo en consola payload
+    Serial.print(content);
+    Serial.println();
+
+    // imprimo en consola topic recibido y topic error configurado en este nodo
+    Serial.println(topic);
+    Serial.println(topic_error);
+
+    // paso string a char
+    char topic_error_buff[100];
+    topic_error.toCharArray(topic_error_buff,topic_error.length()+1);
+
+    // comparo si topic recibido es igual a topic configurado en este nodo
+    int result = strcmp(topic,topic_error_buff);
+    if (result==0)
+    {
+      // topic recibido corresponde a este nodo
+      digitalWrite(led, HIGH);
+      bip(150,3);//bip error
+      delay(200);
+      bip(150,3);//bip error
+      digitalWrite(led, LOW);
+    }
+}
+
+// inicia la comunicacion MQTT
+// inicia establece el servidor y el callback al recibir un mensaje
+void InitMqtt()
+{
+    client.setServer(mqttServer, mqttPort);
+    client.setCallback(OnMqttReceived);
+}
+
+void mqttConnect(){
+  
+
+  client.setServer(mqttServer, mqttPort);
+  
+  while (!client.connected()) {
+      Serial.println("Connecting to MQTT...");
+  
+      // if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+      if (client.connect(NODO_NOMBRE)) {
+
+        
+
+        Serial.print("conectado a mqtt en: ");
+        Serial.print(MQTT_SERVER);
+        Serial.print(":");
+        Serial.print(MQTT_PORT);
+        Serial.println();
+
+        SuscribeMqtt();
+        PublishMqtt();
+
+        
+      } else {
+  
+        Serial.print("failed with state ");
+        Serial.print(client.state());
+        Serial.println(" try again in 2 seconds");
+        delay(2000);
+  
+      }
+  }
+
+    
+  client.publish("esp/test", "Hello from ESP32+RFIDrc522");
+}
+
+void init_setup(){
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+  SPI.begin();
+  rfid.PCD_Init();//Inicilializar lector
+  Serial.begin(115200); // Velocidad del terminal serial
+}
+
+//WIFI SETUP
+void wifi_setup() {
+  
+
+  //wifi
+  const char* ssid = WIFI_LOCAL_SSID;
+  //const char* password = WIFI_LOCAL_PASS;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid);
+
+  
+
+  while (WiFi.status() != WL_CONNECTED) {   
+      Serial.println("Connecting to WiFi..");
+      Serial.println(WiFi.status());
+      Serial.println(ssid);
+      Serial.println();
+      Serial.print("ESP Board MAC Address:  ");
+      Serial.println(WiFi.macAddress());
+      ledblink(300);
+      delay(1000);
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+  Serial.println(WiFi.localIP());
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  ledblink(5000);
+  bip(200,2);//conexion wifi exitosa
+
+  
+  }
+
+
+
+
+
+
+// gestiona la comunicación MQTT
+// comprueba que el cliente está conectado
+// no -> intenta reconectar
+// si -> llama al MQTT loop
+void HandleMqtt()
+{
+    if (!client.connected())
+    {
+        //llamar a coneccion de mqtt
+        mqttConnect();
+    }
+    client.loop();
+}
+
+// únicamente inicia seria, ethernet y MQTT
+void setup()
+{
+    init_setup();
+    wifi_setup();
+    delay(1500);
+    InitMqtt();
+}
+
+// únicamente llama a HandleMqtt
+void loop()
+{
+    HandleMqtt();
 }
